b969cbd56c748eba1a146843cdc5cc16eb6a6329
Sumeet S Adhav <65108927+sumeet0805@users.noreply.github.com>
2023-08-19 09:40:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const port = 1000;
+const port = 9000;
 const path = require("path");
 const app = express();
 app.set("view engine", "ejs");
